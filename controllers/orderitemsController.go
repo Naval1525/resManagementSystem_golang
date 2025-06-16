fffb381ceb167e74c96d279b3f212b7901043175
Naval15 <e23cseu0962@bennett.edu.in>
2025-06-17 01:01:00 +0530

    feat: add order items and table controllers with CRUD operations

--- controllers/orderitemsController.go
+++ controllers/orderitemsController.go
@@ -1,1 +1,40 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Get Users"})
+	}
+}
+
+func GetOrderItemsByOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Get Users by Order"})
+	}
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Get Users"})
+	}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Create Food"})
+	}
+}
+
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Update Food"})
+	}
+}
+
+func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+
+}
--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,1 +1,26 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetTables() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Get Users"})
+	}
+}
+func GetTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Get Users"})
+	}
+}
+
+func CreateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Create Food"})
+	}
+}
+
+func UpdateTable() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Update Food"})
+	}
+}
